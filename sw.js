1242c62f4ebf71a7366d2a486e01c07f0b57fb66
Yolandamujiu <13819274080@163.com>
2024-12-06 23:41:01 +0800

    Site updated: 2024-12-06 23:40:59

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732159752179";
+const VERSION = "1733499658616";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
