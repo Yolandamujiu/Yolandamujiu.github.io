095346cd7a4572518bad4cec1127dbb0a333c143
Yolandamujiu <13819274080@163.com>
2024-12-07 12:51:53 +0800

    Site updated: 2024-12-07 12:51:51

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733546966511";
+const VERSION = "1733547111188";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
